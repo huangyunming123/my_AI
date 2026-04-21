6066413d1d4a47d467e0e10a2d37405f225d0205
huangyunming <469000928@qq.com>
2026-04-22 00:44:57 +0800

    commit

---
+++ springAiAlibaba2.0/SAA2.0-HelloWorld/src/main/java/com/atguigu/SSASEEApplication.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSASEEApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SSASEEApplication.class, args);
+    }
+}
